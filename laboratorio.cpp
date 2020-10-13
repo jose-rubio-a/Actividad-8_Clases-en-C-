2d54635a4d030daef0d6d82e5c58182fadb05485
jose.rubio5317@alumnos.udg.mx <jose.rubio5317@alumnos.udg.mx>
2020-10-13 11:32:52 -0500

    Modificamos la funcion mostrar para imprimir un encabezado y utilizar la sobrecarga de la clase Computadora

--- laboratorio.cpp
+++ laboratorio.cpp
@@ -15,12 +15,19 @@ void Laboratorio::agregarFinal(const Computadora &c){
 }
 
 void Laboratorio::mostrar(){
-    for(size_t i = 0; i < cont; i++){
+    cout << left;
+    cout << setw(16) << "Nombre Equipo";
+    cout << setw(20) << "Sistema Operativo";
+    cout << setw(6) << "RAM";
+    cout << setw(16) << "Microprocesador";
+    cout << endl;
+        for(size_t i = 0; i < cont; i++){
         Computadora &c = computadoras[i]; 
-        cout << "Nombre de Equipo: " << c.getNombreEquipo() << endl;
-        cout << "Sistema Operativo: " << c.getSistemaOperativo() << endl;
-        cout << "Tamano Ram: " << c.getTamanoRam() << endl;
-        cout << "Procesador: " << c.getProcesador() << endl;
-        cout << endl;
+        cout << c;
+        //cout << "Nombre de Equipo: " << c.getNombreEquipo() << endl;
+        //cout << "Sistema Operativo: " << c.getSistemaOperativo() << endl;
+        //cout << "Tamano Ram: " << c.getTamanoRam() << endl;
+        //cout << "Procesador: " << c.getProcesador() << endl;
+        //cout << endl;
     }
 }
